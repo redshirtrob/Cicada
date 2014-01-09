a57af4ca3287929085715fe4398bdacd1790a220
Robert S. Jones <robert.jones.sv@gmail.com>
2014-01-08 21:45:10 -0800

    Initial Commit

---
+++ Lisp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Lisp
+//
+//  Created by Robert Jones on 1/8/14.
+//  Copyright (c) 2014 Robert Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
