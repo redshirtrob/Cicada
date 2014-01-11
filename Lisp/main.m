81aed251c969e79828770de29cf712efe9bec05f
Robert S. Jones <robert.jones.sv@gmail.com>
2014-01-11 12:03:09 -0800

    Change prompt.

--- Lisp/main.m
+++ Lisp/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        RJRepl *repl = [[RJRepl alloc] initWithPrompt:@"rjlisp> "];
+        RJRepl *repl = [[RJRepl alloc] initWithPrompt:@" > "];
         [repl run];
     }
     return 0;
