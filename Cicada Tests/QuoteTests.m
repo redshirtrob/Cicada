bbe63abd393b302430e65cfe05d3883d7f0e0a6f
Robert S. Jones <robert.jones.sv@gmail.com>
2014-02-22 10:23:26 -0800

    Add quote tests.

---
+++ Cicada Tests/QuoteTests.m
@@ -0,0 +1,66 @@
+//
+//  QuoteTests.m
+//  Cicada
+//
+//  Created by Robert Jones on 2/22/14.
+//  Copyright (c) 2014 Robert Jones. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RJScheme.h"
+#import "NSError+RJScheme.h"
+
+static RJScheme *_scheme = nil;
+
+@interface QuoteTests : XCTestCase
+
+@end
+
+@implementation QuoteTests
+
++ (void)setUp
+{
+    _scheme = [[RJScheme alloc] init];
+}
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+// quote
+- (void)testQuote
+{
+    NSString *exp = @"(quote x)";
+    NSString *expectedValue = @"x";
+    NSString *actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+
+    exp = @"(quote ())";
+    expectedValue = @"()";
+    actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+
+    exp = @"(quote (x))";
+    expectedValue = @"(x)";
+    actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+
+    exp = @"(quote (quote x))";
+    expectedValue = @"(quote x)";
+    actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+
+    exp = @"(quote quote)";
+    expectedValue = @"quote";
+    actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+}
+
+@end
