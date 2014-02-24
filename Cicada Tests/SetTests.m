f2898c0d88e394fcb1a68a266076edb8691aa383
Robert S. Jones <robert.jones.sv@gmail.com>
2014-02-23 21:30:24 -0800

    Add set! unit tests.

---
+++ Cicada Tests/SetTests.m
@@ -0,0 +1,71 @@
+//
+//  SetTests.m
+//  Cicada
+//
+//  Created by Robert Jones on 2/23/14.
+//  Copyright (c) 2014 Robert Jones. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "RJScheme.h"
+#import "NSError+RJScheme.h"
+
+static RJScheme *_scheme = nil;
+
+@interface SetTests : XCTestCase
+
+@end
+
+@implementation SetTests
+
++ (void)setUp
+{
+    _scheme = [[RJScheme alloc] init];
+}
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testSet
+{
+    NSString *exp = @"(begin (define x 0) (set! x 1) x)";
+    NSString *expectedValue = @"1";
+    NSString *actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+}
+
+- (void)testSetErrors
+{
+    NSError *error = nil;
+    NSString *exp = @"(set!)";
+    [_scheme evalString:exp error:&error];
+    XCTAssertNotNil(error, @"Function should throw error for expression: %@", exp);
+
+    error = nil;
+    exp = @"(set! y)";
+    [_scheme evalString:exp error:&error];
+    XCTAssertNotNil(error, @"Function should throw error for expression: %@", exp);
+
+    error = nil;
+    exp = @"(set! z 1)";
+    [_scheme evalString:exp error:&error];
+    XCTAssertNotNil(error, @"Function should throw error for expression: %@", exp); // failing
+}
+
+- (void)testSetDisplay
+{
+    NSString *exp = @"set!";
+    NSString *expectedValue = @"#<Syntax set!>";
+    NSString *actualValue = [_scheme testEvalWithString:exp error:nil];
+    XCTAssertEqualObjects(actualValue, expectedValue, @"%@ != %@", exp, actualValue);
+}
+
+@end
