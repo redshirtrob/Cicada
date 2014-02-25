3a0c5cb1065bf0f9559974f4201ed85eb5cd39bb
Robert S. Jones <robert.jones.sv@gmail.com>
2014-02-24 23:58:11 -0800

    Add error checking to multiply function.

--- Cicada/RJMultiplyProcedure.m
+++ Cicada/RJMultiplyProcedure.m
@@ -6,16 +6,27 @@
 //  Copyright (c) 2014 Robert Jones. All rights reserved.
 //
 
+#import "RJScheme.h"
 #import "RJMultiplyProcedure.h"
 
 @implementation RJMultiplyProcedure
 
 - (id)evalWithValues:(NSArray *)values error:(NSError **)error
 {
+    NSError *tmpError;
+
     float prod = 1;
     for (NSNumber *number in values) {
-        prod *= [number floatValue];
+        if ([number isKindOfClass:[NSNumber class]]) {
+            prod *= [number floatValue];
+        }
+        else {
+            tmpError = [NSError rjschemeEvalErrorWithString:[NSString stringWithFormat:@"Error: expected number but got %@", [RJScheme toString:number]]];
+            break;
+        }
     }
+
+    COPY_ERROR(error, tmpError);
     return @(prod);
 }
 
